053ef723aa5780206601ab506cba00dc05b7c719
Raphael <raphaelmalinski@gmail.com>
2020-12-18 10:19:45 -0300

    Creates timeStamp function

--- app/src/main/java/com/example/myapp4all/MainActivity.kt
+++ app/src/main/java/com/example/myapp4all/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.myapp4all
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    private fun timeStamp(): Long {
+        val currentTime = System.currentTimeMillis();
+        return currentTime
     }
 }
