a4d0cb175afc149e933dfcfd32fb52ae36a65e21
Raphael <raphaelmalinski@gmail.com>
2020-12-18 10:44:30 -0300

    Includes necessary classes to the application

--- app/src/main/java/com/example/myapp4all/MainActivity.kt
+++ app/src/main/java/com/example/myapp4all/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.myapp4all
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
