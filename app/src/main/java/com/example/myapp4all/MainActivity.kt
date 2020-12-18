8229faba70a3f6fad775e77e2810ceab9472765b
Raphael <raphaelmalinski@gmail.com>
2020-12-18 10:45:47 -0300

    Creates changeBgColor function

--- app/src/main/java/com/example/myapp4all/MainActivity.kt
+++ app/src/main/java/com/example/myapp4all/MainActivity.kt
@@ -12,8 +12,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    private fun changeBgColor() {
+        val timestamp = timeStamp().toString()
+        val lastChar = timestamp.substring(timestamp.length-1)
+
+        val colorChosed = when (lastChar.toInt()) {
+            0 -> Color.WHITE
+            1 -> Color.BLACK
+            2 -> Color.BLUE
+            3 -> Color.GREEN
+            4 -> Color.parseColor("#FFCBDB") //pink
+            5 -> Color.RED
+            6 -> Color.parseColor("#993399") //purple
+            7 -> Color.YELLOW
+            8 -> Color.GRAY
+            else -> Color.parseColor("#C8A2C8") //lillac
+        }
+
+        //Change the bg color
+        val linearLayout: LinearLayout = findViewById(R.id.linearLayout)
+        linearLayout.setBackgroundColor(colorChosed)
+    }
+
     private fun timeStamp(): Long {
         val currentTime = System.currentTimeMillis();
         return currentTime
     }
+
+
 }
